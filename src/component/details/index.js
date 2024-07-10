2593c74f47759efa15197b58bbf1faa449b69837
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2024-07-10 10:43:43 +0300

    make changes in details componet according last recommendations, but have some troubles in code

--- src/component/details/index.js
+++ src/component/details/index.js
@@ -4,3 +4,18 @@ import {Box} from "../box";
 import {Heading} from "../heading";
 import {ListItem} from "../list-item";
 import guestsLogo from "./svg/guests_logo.svg";
+import bedroomLogo from "./svg/bedroom_logo.svg";
+import bedLogo from "./svg/bed.svg";
+import bathroomLogo from "./svg/bathroom.svg";
+
+export const Details = ({guests, bedrooms, beds, baths}) => {
+    return (
+        <Box shadow classname="details">
+            <Heading border>Деталі властивості:</Heading>
+            <ListItem imageSrc={guestsLogo}>Гостей: {guests}</ListItem>
+            <ListItem imageSrc={bedroomLogo}>Спалень: {bedrooms}</ListItem>
+            <ListItem imageSrc={bedLogo}>Ліжок: {beds}</ListItem>
+            <ListItem imageSrc={bathroomLogo}>Ванних кімнат: {baths}</ListItem>
+        </Box>
+    );
+};
