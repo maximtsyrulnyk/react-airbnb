8859a29de69feac41c6938ac51f310e6fc5753ca
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2024-07-12 10:58:39 +0300

    make changes according last requirements

--- src/component/additional/index.js
+++ src/component/additional/index.js
@@ -1,21 +1,19 @@
 import React from "react";
 import "./index.css";
-import {Box} from "../box";
-import {Heading} from "../heading";
-import {ListItem} from "../list-item";
+import Box from "../box";
+import Heading from "../heading";
+import ListItem from "../list-item";
 
 export const Additional = ({data}) => {
   return (
-    <Box shadow classname="additional">
-      <Heading boarder>Додаткові властивості</Heading>
+    <Box shadow className="additional">
+      <Heading border>Додаткові властивості</Heading>
       <ListItem title="Правила дому">{data.house_rules}</ListItem>
-      <ListItem title="Політика скасування">
-        {data.cancellation_policy}
-        </ListItem>
-      <ListItem title="Місцевий транспорт">{data.local_transporation}</ListItem>
+      <ListItem title="Політика скасування">{data.cancellation_policy}</ListItem>
+      <ListItem title="Місцевий транспорт">{data.local_transportation}</ListItem>
       <ListItem title="Мови хоста">{data.host_languages.map((e) => `${e}, `)}</ListItem>
       <ListItem title="Спеціальні пропозиції:">{data.special_offers}</ListItem>
-      <ListItem title="Інструкції щодо реєстрації">{data.checkin_instructions}</ListItem>
+      <ListItem title="Інструкції щодо реєстрації">{data.instructions}</ListItem>
     </Box>
   );
 };
